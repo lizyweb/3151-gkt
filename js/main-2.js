f3262ff2a638563a85a105a0f80c73f152649c06
lizyweb <147726740+lizyweb@users.noreply.github.com>
2025-03-20 17:14:44 +0530

    Add files via upload

---
+++ js/main-2.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  window.onpageshow = function(event) {
+    if (event.persisted) {
+      location.reload(); // Reload the page when navigated back to
+    }
+  };
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+
+});
---
+++ js/main-5.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+  
+
+    
+           // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // International Tour carousel
+    $(".InternationalTour-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : false,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+
+});
